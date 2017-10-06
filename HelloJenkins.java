8ef03c137adf1bbf27c0a90f15464df478abb286
h1w4y <x90h1w4y@gmail.com>
2017-10-05 19:10:37 -0700

    print 12 times

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,7 +2,7 @@ public class HelloJenkins{
 
   public static void main(String[] args){
 
-    for(int i=1; i<=11;i++){
+    for(int i=1; i<=12;i++){
 
      System.out.println("Hello Jenkins ... "+i);
 
